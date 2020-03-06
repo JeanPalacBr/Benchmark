481276970e11bec716213551103cb2340e5fec07
JeanPalacBr <jeanpalacio@uninorte.edu.co>
2020-03-06 15:16:06 -0500

    primer commit

---
+++ Bench.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: JPBX
+ *
+ * Created on 6 de marzo de 2020, 02:09 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+ 
+int main () 
+{
+    cout << "Hola, mundo ";
+    return 0;
+}
